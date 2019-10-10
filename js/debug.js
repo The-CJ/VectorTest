f06656dbd598d1703a5ceaab964bfbb27a659dbb
The_CJ <cj@tbits.net>
2019-10-10 11:38:01 +0200

    style changes and debug stuff

---
+++ js/debug.js
@@ -0,0 +1,8 @@
+function showDebug() {
+  var x = document.getElementById('debug_console');
+  x.style.display = "block";
+}
+function hideDebug() {
+  var x = document.getElementById('debug_console');
+  x.style.display = "none";
+}
