cc52443fa65bb2a30ce0420a2ccd00b1908df7cb
The_CJ <cj@tbits.net>
2019-10-10 12:28:05 +0200

    Inital Load of area

--- js/area.js
+++ js/area.js
@@ -1,13 +1,21 @@
-class MainArea {
+class PlayArea {
   constructor(x={}) {
     this.area_id = x["area_id"] ? x["area_id"] : "main_area";
     this.HTMLObject = document.getElementById(this.area_id);
-    if (!this.HTMLObject) { throw "cant find #"+this.area_id; }
+    if (!this.HTMLObject) { throw "can't find #"+this.area_id; }
 
     this.objects = x["objects"] ? x["objects"] : [];
   }
 
+  generateObject(n) {
+    var Obj = Utils.getElementByName(n);
+    if (!Obj) { throw "can't find object: "+n; }
+    Obj = new Obj();
+    return this.addObject(Obj);
+  }
+
   addObject(Obj) {
     this.objects.push(Obj);
+    return true;
   }
 }
---
+++ js/master.js
@@ -0,0 +1,15 @@
+var INIT_PLAY_AREA = null;
+document.addEventListener("DOMContentLoaded", function(event) {
+  INIT_PLAY_AREA = new PlayArea({"area_id":"main_area"});
+});
+
+var Utils = new (class {
+  constructor() {
+
+  }
+  getObjectByName(name) {
+    if (name == "circle") { return CircleObject; }
+    // if (name == "rectangle") { return CircleObject; }
+  }
+
+})()
