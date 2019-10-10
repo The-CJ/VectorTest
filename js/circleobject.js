49fa6d33ff8be9bf3b95b473e6759826213b98ce
The_CJ <cj@tbits.net>
2019-10-10 14:41:57 +0200

    notes and possitioning

--- js/circleobject.js
+++ js/circleobject.js
@@ -18,7 +18,8 @@ class CircleObject extends BaseObject {
     this.HTMLObject.style.backgroundColor = this.color;
     this.HTMLObject.style.width = (this.radius * 2)+"px";
     this.HTMLObject.style.height = (this.radius * 2)+"px";
-    this.HTMLObject.style.left = (m_x + this.pos_x)+"px";
-    this.HTMLObject.style.top = (m_y + this.pos_y)+"px";
+    // m_x & m_y is middle of area + pos (-500 -> 0 -> 500...) minus the radius, because we want the middle
+    this.HTMLObject.style.left = ((m_x + this.pos_x) - this.radius)+"px";
+    this.HTMLObject.style.top = ((m_y + this.pos_y) - this.radius)+"px";
   }
 }
