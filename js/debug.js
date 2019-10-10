2272b98ae5e479320282f67a1f1f27abf815ecee
The_CJ <cj@tbits.net>
2019-10-10 14:48:26 +0200

    debug random

--- js/debug.js
+++ js/debug.js
@@ -13,3 +13,20 @@ function debugGenerate() {
   var values = Utils.extractNameValues(ValueNode);
   INIT_PLAY_AREA.generateObject(values["type"], values);
 }
+function debugGenerateRandom() {
+  var r = {
+    "a": Math.floor(Math.random() * 200),
+    "b": Math.floor(Math.random() * 200),
+    "m": Math.floor(Math.random() * 50),
+    "r": Math.floor(Math.random() * 99) + 1,
+    "type": "circle",
+    "v": Math.floor(Math.random() * 15),
+    "vector": {
+      "x": Math.floor(Math.random() * 20),
+      "y": Math.floor(Math.random() * 20)
+    },
+    "x": Math.floor(Math.random() * 600) - 300,
+    "y": Math.floor(Math.random() * 600) - 300
+  };
+  INIT_PLAY_AREA.generateObject(r["type"], r);
+}
