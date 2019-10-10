32996723576edc30c1fc69b555f65090502772f8
The_CJ <cj@tbits.net>
2019-10-10 14:36:13 +0200

    more positioning

--- js/baseobject.js
+++ js/baseobject.js
@@ -3,10 +3,11 @@ class BaseObject {
     this.Area = null;
     this.HTMLObject = null;
     this.bounce = x['bounce'] ? x['bounce'] : false;
-    this.mass = x['m'] ? x['m'] : 0;
-    this.pos_x = x['x'] ? x['x'] : 0;
-    this.pos_y = x['y'] ? x['y'] : 0;
-    this.velocity = x['v'] ? x['v'] : 0;
+    this.mass = x['m'] ? parseInt(x['m']) : 0;
+    this.pos_x = x['x'] ? parseInt(x['x']) : 0;
+    this.pos_y = x['y'] ? parseInt(x['y']) : 0;
+    this.velocity = x['v'] ? parseInt(x['v']) : 0;
+    this.color = x["color"] ? x["color"] : "green";
     this.vector = this.getVector(x["vector"]);
   }
 
@@ -20,10 +21,13 @@ class BaseObject {
       if (!x || !y) { throw "can't generate vector from string"; }
       return {"x":parseFloat(x), "y":parseFloat(y)}
     }
-
   }
 
   generateHTMLObject() {
     throw "can't generate object for base class";
+  }
+
+  update() {
+    throw "can't update base class";
   }
 }
--- js/circleobject.js
+++ js/circleobject.js
@@ -1,12 +1,24 @@
 class CircleObject extends BaseObject {
   constructor(x={}) {
     super(x);
-    this.radius = x['r'] ? x['r'] : 0;
+    this.radius = x['r'] ? parseInt(x['r']) : 1;
   }
 
   generateHTMLObject() {
     // generate from phantom and stick to this.HTMLObject
     this.HTMLObject = document.querySelector("[phantom-models] > .object.circle").cloneNode(true);
     this.Area.HTMLObject.appendChild(this.HTMLObject);
+    this.update();
+  }
+
+  update() {
+    var m_x = this.Area.HTMLObject.offsetWidth / 2;
+    var m_y = this.Area.HTMLObject.offsetHeight / 2;
+
+    this.HTMLObject.style.backgroundColor = this.color;
+    this.HTMLObject.style.width = (this.radius * 2)+"px";
+    this.HTMLObject.style.height = (this.radius * 2)+"px";
+    this.HTMLObject.style.left = (m_x + this.pos_x)+"px";
+    this.HTMLObject.style.top = (m_y + this.pos_y)+"px";
   }
 }
