0b2470d38f831751b5a287cdfaa428fd092cd3a0
The_CJ <cj@tbits.net>
2019-10-10 15:45:19 +0200

    removed m_x and m_y

--- js/circleobject.js
+++ js/circleobject.js
@@ -13,9 +13,6 @@ class CircleObject extends BaseObject {
   }
 
   update() {
-    var m_x = Math.floor(this.Area.HTMLObject.offsetWidth / 2);
-    var m_y = Math.floor(this.Area.HTMLObject.offsetHeight / 2);
-
     // make it smooth
     this.HTMLObject.style.transitionProperty = "top,left";
     this.HTMLObject.style.transitionTimingFunction = "linear";
@@ -25,21 +22,23 @@ class CircleObject extends BaseObject {
     this.pos_x = this.pos_x + this.vector.x;
     this.pos_y = this.pos_y + this.vector.y;
 
-    // wall collision
-    if ( (m_x+this.pos_x-this.radius) <= 0) {
-      // collsion with west site, negate X vector
+    // wall collision | west
+    if ( (this.pos_x-this.radius) <= 0) {
       this.eventCollisionWall("west");
       this.vector.x = (this.vector.x * -1);
     }
-    if (this.Area.HTMLObject.offsetWidth) {  }
+    // wall collision | east
+    if (this.pos_x+this.radius >= this.Area.HTMLObject.offsetWidth) {
+      this.eventCollisionWall("east");
+      this.vector.x = (this.vector.x * -1);
+    }
 
     // updating display
     this.HTMLObject.style.backgroundColor = this.color;
     this.HTMLObject.style.width = (this.radius * 2)+"px";
     this.HTMLObject.style.height = (this.radius * 2)+"px";
-    // m_x & m_y is middle of area + pos (-500 -> 0 -> 500...) minus the radius, because we want the middle
-    this.HTMLObject.style.left = ((m_x + this.pos_x) - this.radius)+"px";
-    this.HTMLObject.style.top = ((m_y + this.pos_y) - this.radius)+"px";
+    this.HTMLObject.style.left = (this.pos_x - this.radius)+"px";
+    this.HTMLObject.style.top = (this.pos_y - this.radius)+"px";
     this.eventUpdate();
   }
 }
